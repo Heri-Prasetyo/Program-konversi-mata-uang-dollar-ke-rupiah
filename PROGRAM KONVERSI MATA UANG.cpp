8e325d394af37594efeafc930edbb0adfe8e2656
Heri Prasetyo <45659342+Heri-Prasetyo@users.noreply.github.com>
2018-12-22 10:11:26 +0700

    Add files via upload

---
+++ PROGRAM KONVERSI MATA UANG.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int Dollar,Rupiah;
+
+cout<<"PROGRAM KONVERSI MATA UANG DOLLAR KE RUPIAH"<<endl;
+
+cout<<"Masukan Nilai Dollar : ";
+cin>>Dollar;
+Dollar=Dollar*15202;
+cout<<"Hasilnya Nilai Rupiah : "<<Dollar;
+return 0;
+
+
+}
